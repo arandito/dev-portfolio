5908ba52a2142e06dc2d088dba944a8f3fa6fe63
Antonio Aranda <antonioaranda0626@gmail.com>
2024-10-15 04:37:52 -0400

    Add new project icons.

--- components/info.tsx
+++ components/info.tsx
@@ -2,7 +2,7 @@ import { MapPin, Laptop, GraduationCap } from "lucide-react"
 
 const Info = () => {
   return (
-    <div className="flex flex-col sm:flex-row gap-2 sm:gap-6 pt-8 pb-2">
+    <div className="flex flex-col sm:flex-row gap-2 sm:gap-6 pt-6 pb-2">
       <div className="flex flex-row items-center">
         <MapPin className="h-5 w-5 mr-2" />
         new york city
